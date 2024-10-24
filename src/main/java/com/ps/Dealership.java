40c669a56429b15f37d6b6cf4607ceb74e96d79f
Bhupa Subedi <bhupa_subedi@yahoo.com>
2024-10-24 16:19:59 -0500

    added vehicle getter and setter

---
+++ src/main/java/com/ps/Dealership.java
@@ -0,0 +1,60 @@
+package com.ps;
+
+import java.util.ArrayList;
+
+public class Dealership {
+
+    private String name;
+    private String address;
+    private String phone;
+    private  ArrayList<Vehicle> inventory;
+
+    public Dealership(String name, String address, String phone, ArrayList<Vehicle> inventory) {
+        this.name = name;
+        this.address = address;
+        this.phone = phone;
+        this.inventory = inventory;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public ArrayList<Vehicle> getInventory() {
+        return inventory;
+    }
+
+    public void setInventory(ArrayList<Vehicle> inventory) {
+        this.inventory = inventory;
+    }
+
+    @Override
+    public String toString() {
+        return "Dealership{" +
+                "name='" + name + '\'' +
+                ", address='" + address + '\'' +
+                ", phone='" + phone + '\'' +
+                ", inventory=" + inventory +
+                '}';
+    }
+}
--- src/main/java/com/ps/UserInterface.java
+++ src/main/java/com/ps/UserInterface.java
@@ -1,4 +1,6 @@
 package com.ps;
 
 public class UserInterface {
+
+
 }
