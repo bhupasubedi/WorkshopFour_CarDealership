b5307615bb0b9bcf1c8380d6d29f14c48859ef79
Bhupa Subedi <bhupa_subedi@yahoo.com>
2024-10-24 15:03:02 -0500

    First commit

---
+++ src/main/java/com/ps/Program.java
@@ -0,0 +1,17 @@
+package com.ps;
+
+public class Program {
+
+    // private static Dealership dealership;
+
+    public static void init() {
+        System.out.println("Init method");
+        //Loading the dealership and inventory from a file
+    }
+
+    public static void display() {
+        init();
+        System.out.println("Load the menu");
+        // Load the menu
+    }
+}
